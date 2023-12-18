0b2562451efebce9e616e273af35c9a6ac9257ba
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2023-12-18 14:51:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        kattdjur bondKatt = new kattdjur(20, 5, true);
+        kattdjur SabelTandadTiger = new kattdjur(100, 200, false);
+        bondKatt.grow(20);
+    }
+}
+
---
+++ src/kattdjur.java
@@ -0,0 +1,12 @@
+public class kattdjur {
+    public kattdjur(int i, int i1, boolean b) {
+        int height = 0;
+        int weight = 0;
+        boolean isAlive = true;
+    }
+    public static int grow(int height){
+        height = height + 1;
+        System.out.println("it grew to " + height);
+        return height;
+    }
+}
